7e349c60c875ef9d8aa5e91e25eb0c1a55698577
haraonline <haragopal.kovvali@gmail.com>
2019-01-08 01:42:25 +0530

    added psycopg2-binary

--- run.py
+++ run.py
@@ -4,6 +4,4 @@ from app.auth.models import User
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
 flask_app.run()
