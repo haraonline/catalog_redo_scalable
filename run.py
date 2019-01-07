1f23ee7a9444ecea335872608739dc36d3aa474d
haraonline <haragopal.kovvali@gmail.com>
2019-01-08 01:48:45 +0530

    try-except-block

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
+from sqlalchemy import exc
 
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-flask_app.run()
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry', email='harry@potters.com', password='secret')
+    except exc.IntegrityError:
+        flask_app.run()
+
